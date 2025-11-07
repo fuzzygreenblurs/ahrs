f561f1b687ee56b396645a8db0df48b66c6579b9
fuzzygreenblurs <akhil.sankar@gmail.com>
2025-11-07 18:44:35 -0500

    remainder of DCM functions and basic tests

--- src/dcm.cpp
+++ src/dcm.cpp
@@ -28,4 +28,45 @@ namespace ahrs {
 
     return true;
   }
+
+  DCM DCM::operator*(const DCM& other) const {
+    return DCM(R_ * other.R_); 
+  }
+
+
+  Vector3f DCM::operator*(const Vector3f& vec) const {
+    return R_ * vec;  
+  }
+
+  DCM DCM::from_euler_zyx(float roll, float pitch, float yaw) {
+    float cr = cos(roll),  sr = sin(roll);
+    float cp = cos(pitch), sp = sin(pitch);
+    float cy = cos(yaw),   sy = sin(yaw);
+    
+    // derived from right-to-left multiplication of euler rotations
+    // ZYX: roll THEN pitch THEN yaw
+    Matrix3f R;
+    R << cy*cp, cy*sp*sr - sy*cr, cy*sp*cr + sy*sr,
+         sy*cp, sy*sp*sr + cy*cr, sy*sp*cr - cy*sr,
+           -sp,            cp*sr,            cp*cr;
+
+    return DCM(R);
+  }
+
+  Vector3f DCM::to_euler_zyx() const {
+    float roll  = atan2(R_(2,1), R_(2,2));
+    float pitch = asin(-R_(2,0));
+    float yaw   = atan2(R_(1,0), R_(0,0));
+
+    return Vector3f(roll, pitch, yaw);
+  }
+
+  DCM DCM::inv() const {
+    return T();
+  }
+
+  DCM DCM::T() const {
+    return DCM(R_.transpose());
+  }
 }
+
--- test/test_dcm.cpp
+++ test/test_dcm.cpp
@@ -47,11 +47,12 @@ namespace ahrs {
     CHECK(mat.isApprox(target, DCM::TOLERANCE));
   }
 
-  TEST(TEST_DCM, assign_ctor) {
+  TEST(TEST_DCM, parametrized_ctor) {
+
     Matrix3f valid;
     float c = cos(M_PI/4);
     float s = sin(M_PI/4);
-
+    
     valid  << 1,0,0,
               0,c,-s,
               0,s,c;
@@ -61,8 +62,100 @@ namespace ahrs {
     
     CHECK(mat.isApprox(valid, DCM::TOLERANCE));
   }
-}
 
+  TEST(TEST_DCM, multiply_composes_rotation) {
+    Matrix3f m1;
+    m1 << 0, -1, 0,
+          1,  0, 0,
+          0,  0, 1; 
+
+    DCM r1(m1);
+    DCM r2(m1);
+
+    DCM res = r1 * r2;
+    
+    Matrix3f exp;
+    exp << -1,  0, 0,
+                 0, -1, 0,
+                 0,  0, 1; 
+
+    CHECK(res.to_matrix().isApprox(exp, DCM::TOLERANCE));
+  }
+
+  TEST(TEST_DCM, mat_vec_multiply) {
+    Matrix3f m;
+    m << 0, -1, 0,
+         1,  0, 0,
+         0,  0, 1; 
+
+    DCM d(m); 
+    Vector3f v(1, 0, 0);
+    Vector3f res = d * v;
+
+    Vector3f exp(0, 1, 0); 
+    CHECK(res.isApprox(exp, DCM::TOLERANCE));
+  }
+
+  TEST(TEST_DCM, mat_inversion) {
+    Matrix3f m;
+    m << 0, -1, 0,
+         1,  0, 0,
+         0,  0, 1; 
+
+    DCM R(m);
+    DCM prod = R * R.inv();
+    CHECK(prod.to_matrix().isApprox(Matrix3f::Identity(),
+                                    DCM::TOLERANCE));
+     
+  }
+
+  TEST(TEST_DCM, mat_transpose) {
+    Matrix3f m;
+    m << 0, -1, 0,
+         1,  0, 0,
+         0,  0, 1; 
+
+    DCM R(m);
+    DCM RT = R.T();
+    CHECK(RT.to_matrix().isApprox(m.transpose(), DCM::TOLERANCE));
+  }
+
+  TEST(TEST_DCM, euler_zyx_eye) {
+    DCM R = DCM::from_euler_zyx(0, 0, 0);
+    CHECK(R.to_matrix().isApprox(Matrix3f::Identity(), DCM::TOLERANCE));
+  } 
+    
+  TEST(TEST_DCM, euler_zyx_90_yaw) {
+    DCM R = DCM::from_euler_zyx(0, 0, M_PI/2);
+    Matrix3f exp;
+    exp << 0, -1, 0,
+           1,  0, 0,
+           0,  0, 1;
+  
+    CHECK(R.to_matrix().isApprox(exp, DCM::TOLERANCE));
+  }
+
+  TEST(TEST_DCM, dcm_eye_yields_zero_euler_angles) {
+    DCM R = DCM::eye(); 
+    Vector3f euler = R.to_euler_zyx();
+
+    DOUBLES_EQUAL(0.0f, euler(0), DCM::TOLERANCE);
+    DOUBLES_EQUAL(0.0f, euler(1), DCM::TOLERANCE);
+    DOUBLES_EQUAL(0.0f, euler(2), DCM::TOLERANCE);
+  }
+
+  TEST(TEST_DCM, euler_roundtrip) {
+    float r = 0.1f; 
+    float p = 0.2f; 
+    float y = 0.3f; 
+
+    DCM R = DCM::from_euler_zyx(r, p, y);
+    Vector3f euler = R.to_euler_zyx();
+    DOUBLES_EQUAL(r, euler(0), DCM::TOLERANCE);
+    DOUBLES_EQUAL(p, euler(1), DCM::TOLERANCE);
+    DOUBLES_EQUAL(y, euler(2), DCM::TOLERANCE);
+  }
+}
 
 int main(int argc, char** argv) {
   return CommandLineTestRunner::RunAllTests(argc, argv);
